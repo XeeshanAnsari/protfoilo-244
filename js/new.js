d77f2262393cd0701269bad14eda8549dca23b32
xeeshan <xeeshan@gmail.com>
2016-10-26 00:09:08 -0700

    design protfolio box while using two functionallity like appreance of mouse over and out

---
+++ js/new.js
@@ -0,0 +1,14 @@
+/* function for change the apperance when mouse over */
+
+function changeProtBoxOver(){
+    var id = document.getElementById("protBox");
+     id.style.opacity = 0.4;
+}
+
+
+/* function for change the apperance when mouse out */
+
+function changeProtBoxOut(){
+    var id = document.getElementById("protBox");
+     id.style.opacity = 1;
+}
